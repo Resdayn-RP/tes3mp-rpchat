77acb2bb0beec7e11df078faca9a17af9c1c8437
HebiKotei <c1yvan@outlook.com>
2023-02-06 13:50:21 +0000

    Fix

--- main.lua
+++ main.lua
@@ -6,6 +6,22 @@ rpChat.localChatRange = 0
 rpChat.oocColour = color.OrangeRed
 rpChat.meColour = color.PaleGoldenRod
 
+local function log(message)
+    tes3mp.LogMessage(enumerations.log.VERBOSE, "[ Mining ]: " .. message)
+end
+
+---@param message table
+---@return string text
+local function concatenateMessage(message)
+    local text = ""
+    for i, j in pairs(message) do
+        if i ~= 1 then
+            text = text .. " " .. j
+        end
+    end
+    return text
+end
+
 ---@param cellDescription integer
 ---@param message string
 function rpChat.SendMessageToCell(cellDescription, message)
@@ -17,27 +33,29 @@ function rpChat.SendMessageToCell(cellDescription, message)
 end
 
 ---@param pid integer PlayerID
----@param message string
+---@param message string|table
 function rpChat.sendLocalMessage(pid, message)
     if not rpChat.enableLocalChat then return end
+    local text = message
     local playerName = Players[pid].name
     local cell = Players[pid].data.location.cell
-
-    rpChat.SendMessageToCell(cell, "\n" .. playerName .. ": " .. message)
+    rpChat.SendMessageToCell(cell, "\n" .. playerName .. ": " .. text)
 end
 
 ---@param pid integer PlayerID
----@param message string
+---@param message table
 function rpChat.meAction(pid, message)
     if not rpChat.enableLocalChat then return end
     local playerName = Players[pid].name
     local cell = Players[pid].data.location.cell
-    rpChat.SendMessageToCell(cell, rpChat.meColour .. "\n *" .. playerName .. " " .. message[2])
+    local text = concatenateMessage(message)
+    rpChat.SendMessageToCell(cell, rpChat.meColour .. "\n *" .. playerName .. " " .. text)
 end
 
 ---@param pid integer PlayerID
 ---@param message string
 function rpChat.sendGlobalMessage(pid, message)
+    log(message)
     tes3mp.SendMessage(pid, message, true)
 end
 
@@ -45,7 +63,8 @@ end
 ---@param message table
 function rpChat.sendOOCMessage(pid, message)
     local playerName = Players[pid].name
-    local text = rpChat.oocColour .. "\n [OOC] " .. playerName .. ": " .. message[2]
+    local message = concatenateMessage(message)
+    local text = "\n" .. rpChat.oocColour .. " [OOC] " .. playerName .. ": " .. message
     rpChat.sendGlobalMessage(pid, text)
 end
 
